bbfd4e8664811947bac1139233556a009ae3b669
solarballss12-a11y <solarballss12@gmail.com>
2026-08-04 16:59:36 +0500

    Create SpecOverrides.java

---
+++ src/main/java/com/example/lspecs/SpecOverrides.java
@@ -0,0 +1,9 @@
+package com.example.lspecs;
+
+public final class SpecOverrides {
+    public static volatile String cpu = null;
+    public static volatile String gpu = null;
+
+    private SpecOverrides() {
+    }
+}
